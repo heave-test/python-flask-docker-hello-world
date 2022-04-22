c79667e41e96a00cb1b671660fdb13d3adf1effc
victimxsss <31512623+victimxsss@users.noreply.github.com>
2022-04-22 12:04:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,21 @@ import os
 
 app = Flask(__name__)
 
+
+# def hello():
+#     return "Flask inside Docker!!"
+
 @app.route("/")
 def hello():
-    return "Flask inside Docker!!"
+     name = request.args['name']
+     return HELLO_HTML.format(
+             name, str(datetime.now()))
 
+ HELLO_HTML = """
+     <html><body>
+         <h1>Hello, {0}!</h1>
+         The time is {1}.
+     </body></html>"""
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
